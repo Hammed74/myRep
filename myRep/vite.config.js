b8b99d65b2e6b088c516e3f1f5c97b78378883a2
Rarri74 <Hammed74@gmail.com>
2024-06-26 23:02:26 -0400

    deleted dist from git ignore

--- myRep/vite.config.js
+++ myRep/vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/myRep/",
   plugins: [react()],
   alias: {
     "@": path.resolve(__dirname, "./src"),
